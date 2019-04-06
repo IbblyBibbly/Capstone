00d0691687a81b158028903d0b63387b479cd76a
seanag88 <44784451+seanag88@users.noreply.github.com>
2019-04-06 17:45:18 -0600

    Add files via upload

---
+++ Oracle/index.php
@@ -0,0 +1,46 @@
+<?php include("nav.php"); ?>
+
+      <head>
+        <title> Edgar Arroyo's Team Management System </title>  
+        <link href="css/style.css" rel = "stylesheet" type = "text/css">  
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>       
+     </head>
+       
+      <div class="box4">
+        
+          <div class="card" >
+            <div class="card-header">Notifications<hr width="100%", color="black"></div>        
+            <div class="img">
+              <a href="index.php"><img src="images/notifBell.png" alt="Notifications"></a>
+            </div>
+          </div>
+
+          <div class="card" >
+            <div class="card-header">Contributions<hr width="100%", color="black"></div>            
+            <div class="img">
+            <a href="index.php"><img src="images/puzzle.png" alt="Notifications"></a>
+            </div>
+          </div>
+
+          <div class="card" >
+            <div class="card-header">Customer Management<hr width="100%", color="black"></div>            
+            <div class="img">
+            <a href="index.php"><img src="images/customerRelationship.png" alt="Notifications"></a>
+            </div>
+          </div>
+
+            <div class="card" >
+              <div class="card-header">Training & Development<hr width="100%", color="black"></div>                    
+              <div class="img">
+              <a href="index.php"><img src="images/training.png" alt="Notifications"></a>
+            </div>
+            </div>
+      </div>
+
+   </div>
+
+   </div>
+</body>
+</html>
---
+++ Oracle/taskcenter.php
@@ -0,0 +1,23 @@
+<?php include("nav.php"); ?>
+
+     <div class="box4">
+          <div class="card" >
+               <div class="card-header">Notifications<hr width="100%", color="black"></div>        
+               <div class="card-body text">
+               <h5 class="card-title">Primary card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+          </div>
+
+          <div class="card" >
+               <div class="card-header">Contributions<hr width="100%", color="black"></div>            
+               <div class="card-body text-primary">
+               <h5 class="card-title">Primary card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+          </div>
+     </div>
+
+   </div>
+</body>
+</html>
