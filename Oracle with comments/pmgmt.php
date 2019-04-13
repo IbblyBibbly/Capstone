8b7c1ab997e3834ab26f113497d266c77dc26b84
IbblyBibbly <44472270+IbblyBibbly@users.noreply.github.com>
2019-04-13 14:02:36 -0600

    Add files via upload

---
+++ Oracle with comments/pmgmt.php
@@ -0,0 +1,34 @@
+<?php include("nav.php"); ?>
+       <!-- Project management page, with multiple project cards.-->
+     <div class="box5">
+           <!-- card for project 1-->
+          <div class="card" >
+            <div class="card-header">Project1<hr width="100%", color="black"></div>        
+            <div class="card-body text">
+              <h5 class="card-title">PID: XXXX <br>Project Name:XXXXX</h5>
+              <p class="card-text"></p>
+            </div>
+          </div>
+           <!-- card for project 2-->
+          <div class="card" >
+            <div class="card-header">Project2<hr width="100%", color="black"></div>        
+            <div class="card-body text">
+              <h5 class="card-title">PID: XXXX <br>Project Name:XXXXX</h5>
+              <p class="card-text"></p>
+            </div>
+          </div>
+           <!-- card for project 3-->
+          <div class="card" >
+            <div class="card-header">Project3<hr width="100%", color="black"></div>        
+            <div class="card-body text">
+              <h5 class="card-title">PID: XXXX <br>Project Name:XXXXX</h5>
+              <p class="card-text"></p>
+            </div>
+          </div>
+           <!--an infinite number of cards can be put in to this page to reference more DB tables -->
+     </div>
+
+</div>
+
+</body>
+</html>
