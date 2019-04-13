1d62d3a86fb23f8a56ef14666f55b076a444e9b9
seanag88 <44784451+seanag88@users.noreply.github.com>
2019-04-13 15:35:05 -0600

    Add files via upload

---
+++ ManageProject.php
@@ -0,0 +1,55 @@
+<?php
+//ip, user, pass, db
+$con=mysqli_connect("128.198.209.178","root","123","oracle");
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <link href="css/mysql.css" rel = "stylesheet" type = "text/css">   
+    <title>Project One</title>
+    <h1>Project One</h1>
+
+    <br>
+    <div class= "box1">
+        <button type="button">Create</button>
+        <button type="button">Update</button>
+        <button type="button">Delete</button>
+    </div>
+
+    <br><br>
+
+    <table>
+        <tr>
+            <th>Table ID</th>
+            <th>Contributions</th> 
+            <th>KmArticles</th> 
+            <th>ODMCEngagements</th>
+            <th>DQCRRCA</th>
+            <th>BestPCustEngagements</th>
+            <th>LongRunning</th>
+            <th>SRQuality</th>
+            <th>AccountSRNRD</th>
+            <th>CriticalWorkSRSNRD</th> 
+        </tr>
+    <?php
+    
+        $sql = "SELECT * FROM project2";
+        $result = mysqli_query($con, $sql);
+        while ($row = mysqli_fetch_assoc($result)) { 
+            echo "<tr>";
+            foreach ($row as $field => $value) { 
+                echo "<td>" . $value . '<br> <input type="text" name="td" value= "" size="10px">' . "</td>"; 
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+
+
+        echo '<br><br>
+                <button class= "updateButton"type="button">Update</button>
+             '
+               
+    ?>
+</body>
+</html>
